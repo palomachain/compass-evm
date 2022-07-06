7ac4c1d02f28ebde504c72a5a7345bd0dba30ce8
Matija Martinic <matija.martinic@gmail.com>
2022-07-06 15:48:29 +0200

    Deploy EVM valset

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,10 +1,56 @@
-from brownie import accounts, Turnstone
+import json
+import subprocess
+
+from brownie import Turnstone, accounts
 from eth_abi import encode_abi
 
+
+def get_valset(node, valset_id=None):
+    if valset_id is None:
+        valset_id = 99999999
+    process = subprocess.Popen(
+        [
+            "palomad",
+            "--node",
+            node,
+            "--output",
+            "json",
+            "query",
+            "evm",
+            "get-valset-by-id",
+            str(valset_id),
+            "eth-main",
+        ],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+
+    stdout, stderr = process.communicate()
+
+    stdout = stdout.read()
+    stderr = stderr.read()
+
+    if "item not found in store" in stdout or "item not found in store" in stderr:
+        return get_valset(node, valset_id // 2)
+    valset = json.loads(stdout)
+
+    if len(valset.validators) == 0:
+        return get_valset(node, valset_id + valset_id // 2)
+
+    # validators are strings returned like this: "0xabcd...."
+    return valset.validators, valset.powers, valset.valsetID
+
+
 def main():
+    validators, powers, valset_id = get_valset("tcp://localhost:26657")
     acct = accounts.load("deployer_account")
-    validators = [] # should update
-    powers = [] # should update
-    valset_id = 0 # should update
-    turnstone_id = b"ETH_01" # should update
-    Turnstone.deploy(encode_abi(["bytes32"], [turnstone_id]), [validators, powers, valset_id],{"from": acct})
+    turnstone_id = b""  # should update
+    Turnstone.deploy(
+        encode_abi(["bytes32"], [turnstone_id]),
+        [validators, powers, valset_id],
+        {"from": acct},
+    )
+
+
+if __name__ == "__main__":
+    main()
