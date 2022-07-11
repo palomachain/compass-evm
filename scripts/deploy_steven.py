a29b4d51d8b38dfc4d047728361c1119f76752ee
webelf101 <m63974529@gmail.com>
2022-07-12 03:09:56 +0800

    deploy script test

--- scripts/deploy_steven.py
+++ scripts/deploy_steven.py
@@ -6,7 +6,7 @@ from brownie import Turnstone, accounts
 from eth_abi import encode_abi
 
 
-def get_valset(node, valset_id=None):
+def get_valset(node):
     process = subprocess.Popen(
         [
             "palomad",
@@ -25,30 +25,18 @@ def get_valset(node, valset_id=None):
     )
 
     stdout, stderr = process.communicate()
-    # print(stdout)
-    # stdout = str(stdout)
-    # stderr = str(stderr)
-
-    # print(stdout)
 
     valset = json.loads(stdout)
     valset = valset["valset"]
-    print(valset)
     # validators are strings returned like this: "0xabcd...."
     return valset["validators"], valset["powers"], valset["valsetID"]
 
 
 def main():
-    # node = sys.argv[1]
-    # assert node, "must provide 1 argument which is the node: e.g. tcp://localhost:26657"
     node = "tcp://localhost:26657"
     validators, powers, valset_id = get_valset(node)
 
-    # power_sum = sum(powers)
-
-    # assert power_sum >= 2863311530, f"not enough power to reach consensus: {power_sum}"
-
-    acct = accounts[0]
+    acct = accounts.load("deployer_account")
     turnstone_id = b""  # should update
     Turnstone.deploy(
         encode_abi(["bytes32"], [turnstone_id]),
